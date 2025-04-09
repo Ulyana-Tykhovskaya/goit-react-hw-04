2e4ffe3e04f3f1566a5b3936197ada52aebfcb33
Ulyana <158500343+Ulyana-Tykhovskaya@users.noreply.github.com>
2025-04-09 21:06:46 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-// import axios from "axios";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
-// const ACCESS_KEY = "DUzP1R9et76guLf7mdGqb9d4SDDs7apvOPC38J2blGk";
-// const BASE_URL = "https://api.unsplash.com/search/photos";
+const App = () => {
+  const [hits, setHits] = useState([]);
+  useEffect(() => {
+    axios.get("https://api.unsplash.com/search/photos").then((res) => {
+      setHits(res.data.hits);
+    });
+  }, []);
+  return <header></header>;
+};
 
-// export default function App()fetchImages = async (query, page = 1, perPage = 12) => {
-//   const response = await axios.get(BASE_URL, {
-//     params: {
-//       query,
-//       page,
-//       per_page: perPage,
-//       client_id: ACCESS_KEY,
-//     },
-//   });
-//   return response.data;
-// };
+export default App;
